1e90a1c6387c022bcb753e26ba14ae8336e2341e
pisha <pisha@hotmail.fr>
2020-09-22 13:58:39 +0200

    création de la classe player qui etend la classe sprite

--- app.js
+++ app.js
@@ -118,6 +118,51 @@ let gameManager = {
 
     }
 
+    /* EXTENSIONS DE CLASSE */
+
+    class Player extends Sprite {
+        constructor(divName, position, assetDesc) {
+            super(divName, position, assetDesc.fileName,
+                new Size(assetDesc.width, assetDesc.height));
+                this.lives = gameSettings.playerstartLives;
+                this.score = 0;
+                this.highScore = 0;
+                this.state = gameSettings.playerState.ok;
+        }
+
+        reset() {
+            this.state = gameSettings.playerState.ok;
+            this.score = 0;
+            this.lives = gameSettings.playerstartLives;
+            this.setLives();
+            this.setScore();
+            this.setHighScore();
+            this.setPosition(gameManager.playerStart.x, gameManager.playerStart.y, true);
+        }
+
+        incrementScore(amount) {
+            this.score += amount;
+            this.setScore();
+            this.setHighScore();
+        }
+
+        setLives() {
+            $('#lives').text('x ' + this.lives);
+        }
+
+        setScore() {
+            $('#score').text(this.score);
+        }
+
+        setHighScore() {
+            if(this.score > this.highScore) {
+                this.highScore = this.score;
+            }
+            $('#highscore').text(this.highscore);
+        }
+
+    }
+
 
 /* FONCTIONS */
 function resetPlayer() {

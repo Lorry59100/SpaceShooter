cdc7f3f7438618b619f471b62cc579a7278c598c
pisha <pisha@hotmail.fr>
2020-09-23 13:11:46 +0200

    ajout de la classe BulletCollection

--- app.js
+++ app.js
@@ -165,6 +165,49 @@ let gameManager = {
         }
     }
 
+    class BulletCollection {
+        constructor(player) {
+            this.listBullets = [];
+            this.lastAdded = 0;
+            this.player = player;
+            this.total_bullets = 0;
+        }
+
+        reset() {
+            for(let i = 0; i < this.listBullets.length; i++) {
+                this.listBullets[i].removeFromBoard();
+            }
+            this.listBullets = [];
+            this.lastAdded = 0;
+            this.total_bullets = 0;
+        }
+
+        update(dt) {
+            for(let i = this.listBullets.length - 1; i >=0 ;i--) {
+                if(this.listBullets[i].dead ==true ) {
+                    this.listBullets.splice(i, 1);
+                } else {
+                    this.listBullets[i].update(dt);
+                }
+            }
+            this.lastAdded += dt;
+
+            if(this.lastAdded > gameSettings.bulletFireRate &&
+                this.player.state != gameSettings.playerState.hitFlashing) {
+                    this.lastAdded = 0;
+                    this.listBullets.push(
+                        new Bullet(
+                            'bullet_' + this.total_bullets,
+                            gameManager.assets['beam'],
+                            new Point(this.player.position.x +(this.player.size.width / 2),
+                            this.player.position.y)
+                        )
+                    );
+                }
+        }
+
+    }
+
     /* EXTENSIONS DE CLASSE */
 
     class Player extends Sprite {
@@ -251,7 +294,6 @@ let gameManager = {
         }
 
     }
-
 
 /* FONCTIONS */
 function tick() {
